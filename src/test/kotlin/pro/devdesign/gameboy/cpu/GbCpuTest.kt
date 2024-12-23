21f6e2470967ec14e4d36c7316e95e3109812695
Alexey Nikitin <nikialeksey@gmail.com>
2024-12-23 22:13:19 +0400

    Added last tests, but some of them are failing

--- src/test/kotlin/pro/devdesign/gameboy/cpu/GbCpuTest.kt
+++ src/test/kotlin/pro/devdesign/gameboy/cpu/GbCpuTest.kt
@@ -20,11 +20,16 @@ internal class GbCpuTest {
     @ParameterizedTest
     @ValueSource(strings = [
         "/cpu-instrs/01-special.gb",
+        "/cpu-instrs/02-interrupts.gb",
         "/cpu-instrs/03-op sp,hl.gb",
         "/cpu-instrs/04-op r,imm.gb",
         "/cpu-instrs/05-op rp.gb",
         "/cpu-instrs/06-ld r,r.gb",
-        "/cpu-instrs/07-jr,jp,call,ret,rst.gb"
+        "/cpu-instrs/07-jr,jp,call,ret,rst.gb",
+        "/cpu-instrs/08-misc instrs.gb",
+        "/cpu-instrs/09-op r,r.gb",
+        "/cpu-instrs/10-bit ops.gb",
+        "/cpu-instrs/11-op a,(hl).gb",
     ])
     fun cpuInstrsIndividual(gbFileName: String) {
         val serial = BufferSerial()
