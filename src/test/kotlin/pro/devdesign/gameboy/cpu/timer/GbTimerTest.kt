ae9f8114ec0aea93222cc57a45194f477aac2ed2
Alexey Nikitin <nikialeksey@gmail.com>
2025-01-07 15:59:14 +0400

    Timer unit test

---
+++ src/test/kotlin/pro/devdesign/gameboy/cpu/timer/GbTimerTest.kt
@@ -0,0 +1,137 @@
+package pro.devdesign.gameboy.cpu.timer
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import pro.devdesign.gameboy.cpu.interrupts.GbInterrupts
+
+class GbTimerTest {
+    @Test
+    fun incrementEvery16Ticks() {
+        val interrupts = GbInterrupts()
+        val timer = GbTimer(
+            interrupts,
+            div = 0x00,
+            tma = 0xFF,
+            tac = 0b101, // tima will increment every 16 ticks
+            tima = 0x00,
+        )
+
+        timer.tick(4)
+        Assertions.assertEquals(0, timer.tima())
+        Assertions.assertEquals(0, timer.div())
+
+        timer.tick(4)
+        Assertions.assertEquals(0, timer.tima())
+        Assertions.assertEquals(0, timer.div())
+
+        timer.tick(4)
+        Assertions.assertEquals(0, timer.tima())
+        Assertions.assertEquals(0, timer.div())
+
+        timer.tick(4)
+        Assertions.assertEquals(1, timer.tima())
+        Assertions.assertEquals(0, timer.div())
+    }
+
+    @Test
+    fun incrementEvery64Ticks() {
+        val interrupts = GbInterrupts()
+        val timer = GbTimer(
+            interrupts,
+            div = 0x00,
+            tma = 0xFF,
+            tac = 0b110,
+            tima = 0x00,
+        )
+
+        repeat(15) {
+            timer.tick(4)
+            Assertions.assertEquals(0, timer.tima())
+            Assertions.assertEquals(0, timer.div())
+        }
+
+        timer.tick(4)
+        Assertions.assertEquals(1, timer.tima())
+        Assertions.assertEquals(0, timer.div())
+    }
+
+    @Test
+    fun incrementEvery256Ticks() {
+        val interrupts = GbInterrupts()
+        val timer = GbTimer(
+            interrupts,
+            div = 0x00,
+            tma = 0xFF,
+            tac = 0b111,
+            tima = 0x00,
+        )
+
+        repeat(63) {
+            timer.tick(4)
+            Assertions.assertEquals(0, timer.tima())
+            Assertions.assertEquals(0, timer.div())
+        }
+
+        timer.tick(4)
+        Assertions.assertEquals(1, timer.tima())
+        Assertions.assertEquals(1, timer.div())
+    }
+
+    @Test
+    fun incrementEvery1024Ticks() {
+        val interrupts = GbInterrupts()
+        val timer = GbTimer(
+            interrupts,
+            div = 0x00,
+            tma = 0xFF,
+            tac = 0b100,
+            tima = 0x00,
+        )
+
+        repeat(63) {
+            timer.tick(4)
+            Assertions.assertEquals(0, timer.tima())
+            Assertions.assertEquals(0, timer.div())
+        }
+
+        timer.tick(4)
+        Assertions.assertEquals(0, timer.tima())
+        Assertions.assertEquals(1, timer.div())
+
+        // 256 cycles done above
+
+        repeat(63) {
+            timer.tick(4)
+            Assertions.assertEquals(0, timer.tima())
+            Assertions.assertEquals(1, timer.div())
+        }
+
+        timer.tick(4)
+        Assertions.assertEquals(0, timer.tima())
+        Assertions.assertEquals(2, timer.div())
+
+        // 512 cycles done above
+
+        repeat(63) {
+            timer.tick(4)
+            Assertions.assertEquals(0, timer.tima())
+            Assertions.assertEquals(2, timer.div())
+        }
+
+        timer.tick(4)
+        Assertions.assertEquals(0, timer.tima())
+        Assertions.assertEquals(3, timer.div())
+
+        // 512+256 cycles done above
+
+        repeat(63) {
+            timer.tick(4)
+            Assertions.assertEquals(0, timer.tima())
+            Assertions.assertEquals(3, timer.div())
+        }
+
+        timer.tick(4)
+        Assertions.assertEquals(1, timer.tima())
+        Assertions.assertEquals(4, timer.div())
+    }
+}
