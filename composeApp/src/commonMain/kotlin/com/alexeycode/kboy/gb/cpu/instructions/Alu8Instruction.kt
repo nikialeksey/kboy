18a124e080966605a0d9765bedfdfecc39c5845e
Alexey Nikitin <nikialeksey@gmail.com>
2025-02-16 00:43:33 +0400

    Optimizations...

--- composeApp/src/commonMain/kotlin/com/alexeycode/kboy/gb/cpu/instructions/Alu8Instruction.kt
+++ composeApp/src/commonMain/kotlin/com/alexeycode/kboy/gb/cpu/instructions/Alu8Instruction.kt
@@ -38,118 +38,92 @@ class Alu8Instruction(
             0x3D -> decrement({ r.a().get() }, { r.a().set(it) }, 4)
 
             // 8-bit add
-            0x80 -> add({ r.a().get() }, { r.b().get() }, { r.a().set(it) }, 4)
-            0x81 -> add({ r.a().get() }, { r.c().get() }, { r.a().set(it) }, 4)
-            0x82 -> add({ r.a().get() }, { r.d().get() }, { r.a().set(it) }, 4)
-            0x83 -> add({ r.a().get() }, { r.e().get() }, { r.a().set(it) }, 4)
-            0x84 -> add({ r.a().get() }, { r.h().get() }, { r.a().set(it) }, 4)
-            0x85 -> add({ r.a().get() }, { r.l().get() }, { r.a().set(it) }, 4)
-            0x86 -> add({ r.a().get() }, { memory.read8(r.hl().get()) }, { r.a().set(it) }, 8)
-            0x87 -> add({ r.a().get() }, { r.a().get() }, { r.a().set(it) }, 4)
-            0xC6 -> add({ r.a().get() }, { operands[1].read8(memory, r) }, { r.a().set(it) }, 8)
+            0x80 -> add({ r.a().get() }, { r.b().get() }, 4)
+            0x81 -> add({ r.a().get() }, { r.c().get() }, 4)
+            0x82 -> add({ r.a().get() }, { r.d().get() }, 4)
+            0x83 -> add({ r.a().get() }, { r.e().get() }, 4)
+            0x84 -> add({ r.a().get() }, { r.h().get() }, 4)
+            0x85 -> add({ r.a().get() }, { r.l().get() }, 4)
+            0x86 -> add({ r.a().get() }, { memory.read8(r.hl().get()) }, 8)
+            0x87 -> add({ r.a().get() }, { r.a().get() }, 4)
+            0xC6 -> add({ r.a().get() }, { operands[1].read8(memory, r) }, 8)
 
             // 8-bit adc
-            in 0x88..0x8F, 0xCE -> {
-                val a = operands[0].read8(memory, r)
-                val n = operands[1].read8(memory, r)
-                val c = if (r.flag().c().isEnabled()) 1 else 0
-                val result = (a + n + c).and(0xFF)
-                operands[0].write8(memory, r, result)
+            0x88 -> adc({ r.a().get() }, { r.b().get() }, 4)
+            0x89 -> adc({ r.a().get() }, { r.c().get() }, 4)
+            0x8A -> adc({ r.a().get() }, { r.d().get() }, 4)
+            0x8B -> adc({ r.a().get() }, { r.e().get() }, 4)
+            0x8C -> adc({ r.a().get() }, { r.h().get() }, 4)
+            0x8D -> adc({ r.a().get() }, { r.l().get() }, 4)
+            0x8E -> adc({ r.a().get() }, { memory.read8(r.hl().get()) }, 8)
+            0x8F -> adc({ r.a().get() }, { r.a().get() }, 4)
+            0xCE -> adc({ r.a().get() }, { operands[1].read8(memory, r) }, 8)
 
-                r.flag().z().setEnabled(result == 0)
-                r.flag().n().disable()
-                r.flag().h().setEnabled((a.and(0x0F) + n.and(0x0F) + c) > 0x0F)
-                r.flag().c().setEnabled((a.and(0xFF) + n.and(0xFF) + c) > 0xFF)
-
-                meta.cycles().action()
-            }
             // 8-bit sub
-            in 0x90..0x97, 0xD6 -> {
-                val a = operands[0].read8(memory, r)
-                val n = operands[1].read8(memory, r)
-                val result = (a - n).and(0xFF)
-                operands[0].write8(memory, r, result)
+            0x90 -> sub({ r.a().get() }, { r.b().get() }, 4)
+            0x91 -> sub({ r.a().get() }, { r.c().get() }, 4)
+            0x92 -> sub({ r.a().get() }, { r.d().get() }, 4)
+            0x93 -> sub({ r.a().get() }, { r.e().get() }, 4)
+            0x94 -> sub({ r.a().get() }, { r.h().get() }, 4)
+            0x95 -> sub({ r.a().get() }, { r.l().get() }, 4)
+            0x96 -> sub({ r.a().get() }, { memory.read8(r.hl().get()) }, 8)
+            0x97 -> sub({ r.a().get() }, { r.a().get() }, 4)
+            0xD6 -> sub({ r.a().get() }, { operands[1].read8(memory, r) }, 8)
 
-                r.flag().z().setEnabled(result == 0)
-                r.flag().n().enable()
-                r.flag().h().setEnabled((a.and(0x0F) - n.and(0x0F)) < 0)
-                r.flag().c().setEnabled((a.and(0xFF) - n.and(0xFF)) < 0)
-
-                meta.cycles().action()
-            }
             // 8-bit sbc
-            in 0x98..0x9F, 0xDE -> {
-                val a = operands[0].read8(memory, r)
-                val n = operands[1].read8(memory, r)
-                val c = if (r.flag().c().isEnabled()) 1 else 0
-                val result = (a - n - c).and(0xFF)
-                operands[0].write8(memory, r, result)
+            0x98 -> sbc({ r.a().get() }, { r.b().get() }, 4)
+            0x99 -> sbc({ r.a().get() }, { r.c().get() }, 4)
+            0x9A -> sbc({ r.a().get() }, { r.d().get() }, 4)
+            0x9B -> sbc({ r.a().get() }, { r.e().get() }, 4)
+            0x9C -> sbc({ r.a().get() }, { r.h().get() }, 4)
+            0x9D -> sbc({ r.a().get() }, { r.l().get() }, 4)
+            0x9E -> sbc({ r.a().get() }, { memory.read8(r.hl().get()) }, 8)
+            0x9F -> sbc({ r.a().get() }, { r.a().get() }, 4)
+            0xDE -> sbc({ r.a().get() }, { operands[1].read8(memory, r) }, 8)
 
-                r.flag().z().setEnabled(result == 0)
-                r.flag().n().enable()
-                r.flag().h().setEnabled((a.and(0x0F) - n.and(0x0F) - c) < 0)
-                // TODO interesting, but here C flag is not always updated:
-                // https://github.com/gbdev/gb-opcodes/commit/d091dd1d4a4b9fe783b53d19a8b0c50a28c0c92c
-                r.flag().c().setEnabled((a.and(0xFF) - n.and(0xFF) - c) < 0)
-
-                meta.cycles().action()
-            }
             // 8-bit and
-            in 0xA0..0xA7, 0xE6 -> {
-                val a = r.a().get()
-                val n = operands[1].read8(memory, r)
-                val result = a.and(n)
-                operands[0].write8(memory, r, result)
+            0xA0 -> and({ r.b().get() }, 4)
+            0xA1 -> and({ r.c().get() }, 4)
+            0xA2 -> and({ r.d().get() }, 4)
+            0xA3 -> and({ r.e().get() }, 4)
+            0xA4 -> and({ r.h().get() }, 4)
+            0xA5 -> and({ r.l().get() }, 4)
+            0xA6 -> and({ memory.read8(r.hl().get()) }, 8)
+            0xA7 -> and({ r.a().get() }, 4)
+            0xE6 -> and({ operands[1].read8(memory, r) }, 8)
 
-                r.flag().z().setEnabled(result == 0)
-                r.flag().n().disable()
-                r.flag().h().enable()
-                r.flag().c().disable()
-
-                meta.cycles().action()
-            }
             // 8-bit xor
-            in 0xA8..0xAF, 0xEE -> {
-                val a = r.a().get()
-                val n = operands[1].read8(memory, r)
-                val result = a.xor(n)
-                operands[0].write8(memory, r, result)
+            0xA8 -> xor({ r.b().get() }, 4)
+            0xA9 -> xor({ r.c().get() }, 4)
+            0xAA -> xor({ r.d().get() }, 4)
+            0xAB -> xor({ r.e().get() }, 4)
+            0xAC -> xor({ r.h().get() }, 4)
+            0xAD -> xor({ r.l().get() }, 4)
+            0xAE -> xor({ memory.read8(r.hl().get()) }, 8)
+            0xAF -> xor({ r.a().get() }, 4)
+            0xEE -> xor({ operands[1].read8(memory, r) }, 8)
 
-                r.flag().z().setEnabled(result == 0)
-                r.flag().n().disable()
-                r.flag().h().disable()
-                r.flag().c().disable()
-
-                meta.cycles().action()
-            }
             // 8-bit or
-            in 0xB0..0xB7, 0xF6 -> {
-                val a = r.a().get()
-                val n = operands[1].read8(memory, r)
-                val result = a.or(n)
-                r.a().set(result)
-                operands[0].write8(memory, r, result)
+            0xB0 -> or({ r.b().get() }, 4)
+            0xB1 -> or({ r.c().get() }, 4)
+            0xB2 -> or({ r.d().get() }, 4)
+            0xB3 -> or({ r.e().get() }, 4)
+            0xB4 -> or({ r.h().get() }, 4)
+            0xB5 -> or({ r.l().get() }, 4)
+            0xB6 -> or({ memory.read8(r.hl().get()) }, 8)
+            0xB7 -> or({ r.a().get() }, 4)
+            0xF6 -> or({ operands[1].read8(memory, r) }, 8)
 
-                r.flag().z().setEnabled(result == 0)
-                r.flag().n().disable()
-                r.flag().h().disable()
-                r.flag().c().disable()
-
-                meta.cycles().action()
-            }
             // 8-bit cp
-            in 0xB8..0xBF, 0xFE -> {
-                val a = operands[0].read8(memory, r)
-                val n = operands[1].read8(memory, r)
-                val result = (a - n).and(0xFF)
-
-                r.flag().z().setEnabled(result == 0)
-                r.flag().n().enable()
-                r.flag().h().setEnabled((a.and(0x0F) - n.and(0x0F)) < 0)
-                r.flag().c().setEnabled((a.and(0xFF) - n.and(0xFF)) < 0)
-
-                meta.cycles().action()
-            }
+            0xB8 -> cp({ r.b().get() }, 4)
+            0xB9 -> cp({ r.c().get() }, 4)
+            0xBA -> cp({ r.d().get() }, 4)
+            0xBB -> cp({ r.e().get() }, 4)
+            0xBC -> cp({ r.h().get() }, 4)
+            0xBD -> cp({ r.l().get() }, 4)
+            0xBE -> cp({ memory.read8(r.hl().get()) }, 8)
+            0xBF -> cp({ r.a().get() }, 4)
+            0xFE -> cp({ operands[1].read8(memory, r) }, 8)
 
             else -> {
                 0
@@ -189,16 +163,117 @@ class Alu8Instruction(
         return cycles
     }
 
-    private inline fun add(read1: () -> Int, read2: () -> Int, write: (Int) -> Unit, cycles: Int): Int {
+    private inline fun add(read1: () -> Int, read2: () -> Int, cycles: Int): Int {
         val a = read1()
         val n = read2()
         val result = (a + n).and(0xFF)
-        write(result)
+        r.a().set(result)
 
         r.flag().z().setEnabled(result == 0)
         r.flag().n().disable()
         r.flag().h().setEnabled((a.and(0x0F) + n.and(0x0F)) > 0x0F)
         r.flag().c().setEnabled((a.and(0xFF) + n.and(0xFF)) > 0xFF)
+
+        return cycles
+    }
+
+    private inline fun adc(read1: () -> Int, read2: () -> Int, cycles: Int): Int {
+        val a = read1()
+        val n = read2()
+        val c = if (r.flag().c().isEnabled()) 1 else 0
+        val result = (a + n + c).and(0xFF)
+        r.a().set(result)
+
+        r.flag().z().setEnabled(result == 0)
+        r.flag().n().disable()
+        r.flag().h().setEnabled((a.and(0x0F) + n.and(0x0F) + c) > 0x0F)
+        r.flag().c().setEnabled((a.and(0xFF) + n.and(0xFF) + c) > 0xFF)
+
+        return cycles
+    }
+
+    private inline fun sub(read1: () -> Int, read2: () -> Int, cycles: Int): Int {
+        val a = read1()
+        val n = read2()
+        val result = (a - n).and(0xFF)
+        r.a().set(result)
+
+        r.flag().z().setEnabled(result == 0)
+        r.flag().n().enable()
+        r.flag().h().setEnabled((a.and(0x0F) - n.and(0x0F)) < 0)
+        r.flag().c().setEnabled((a.and(0xFF) - n.and(0xFF)) < 0)
+
+        return cycles
+    }
+
+    private inline fun sbc(read1: () -> Int, read2: () -> Int, cycles: Int): Int {
+        val a = read1()
+        val n = read2()
+        val c = if (r.flag().c().isEnabled()) 1 else 0
+        val result = (a - n - c).and(0xFF)
+        r.a().set(result)
+
+        r.flag().z().setEnabled(result == 0)
+        r.flag().n().enable()
+        r.flag().h().setEnabled((a.and(0x0F) - n.and(0x0F) - c) < 0)
+        // TODO interesting, but here C flag is not always updated:
+        // https://github.com/gbdev/gb-opcodes/commit/d091dd1d4a4b9fe783b53d19a8b0c50a28c0c92c
+        r.flag().c().setEnabled((a.and(0xFF) - n.and(0xFF) - c) < 0)
+
+        return cycles
+    }
+
+    private inline fun and(read: () -> Int, cycles: Int): Int {
+        val a = r.a().get()
+        val n = read()
+        val result = a.and(n)
+        r.a().set(result)
+
+        r.flag().z().setEnabled(result == 0)
+        r.flag().n().disable()
+        r.flag().h().enable()
+        r.flag().c().disable()
+
+        return cycles
+    }
+
+    private inline fun xor(read: () -> Int, cycles: Int): Int {
+        val a = r.a().get()
+        val n = read()
+        val result = a.xor(n)
+        r.a().set(result)
+
+        r.flag().z().setEnabled(result == 0)
+        r.flag().n().disable()
+        r.flag().h().disable()
+        r.flag().c().disable()
+
+        return cycles
+    }
+
+    private inline fun or(read: () -> Int, cycles: Int): Int {
+        val a = r.a().get()
+        val n = read()
+        val result = a.or(n)
+        r.a().set(result)
+
+        r.flag().z().setEnabled(result == 0)
+        r.flag().n().disable()
+        r.flag().h().disable()
+        r.flag().c().disable()
+
+        return cycles
+    }
+
+    private inline fun cp(read: () -> Int, cycles: Int): Int {
+        val a = r.a().get()
+        val n = read()
+        val result = (a - n).and(0xFF)
+
+        r.flag().z().setEnabled(result == 0)
+        r.flag().n().enable()
+        r.flag().h().setEnabled((a.and(0x0F) - n.and(0x0F)) < 0)
+        r.flag().c().setEnabled((a.and(0xFF) - n.and(0xFF)) < 0)
 
         return cycles
     }
