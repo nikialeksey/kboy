c2928462a9ab1374473082e7d0725f0ad9ef01d6
Alexey Nikitin <nikialeksey@gmail.com>
2025-02-23 02:08:19 +0400

    Correct rendering on android

--- composeApp/src/androidMain/kotlin/com/alexeycode/kboy/ui/gl/RenderImage.android.kt
+++ composeApp/src/androidMain/kotlin/com/alexeycode/kboy/ui/gl/RenderImage.android.kt
@@ -6,19 +6,18 @@ import android.opengl.GLES20.GL_RGBA
 import android.opengl.GLES20.GL_UNSIGNED_BYTE
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.alexeycode.kboy.gb.ppu.Screen
-import java.io.IOException
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
-import java.nio.FloatBuffer
-import java.nio.ShortBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -50,15 +49,13 @@ private const val COORDINATES_PER_VERTEX = 2
 private const val VERTEX_STRIDE: Int = COORDINATES_PER_VERTEX * 4
 
 private val QUADRANT_COORDINATES = floatArrayOf(
-    //x,    y
-    -1f, 1f,
-    -1f, -1f,
-    1f, -1f,
-    1f, 1f,
+    0f,   144f,
+    0f,   0f,
+    160f, 0f,
+    160f, 144f
 )
 
 private val TEXTURE_COORDINATES = floatArrayOf(
-    //x,    y
     0.0f, 1.0f,
     0.0f, 0.0f,
     1.0f, 0.0f,
@@ -82,23 +79,19 @@ class ScreenGLRenderer(private val screen: () -> Screen) : GLSurfaceView.Rendere
     private var program: Int = -1
     private val textureUnit = IntArray(1)
 
-    /**
-     * Convert float array to float buffer
-     */
-    private val quadrantCoordinatesBuffer: FloatBuffer = ByteBuffer.allocateDirect(
-        QUADRANT_COORDINATES.size * 4).run {
-        order(ByteOrder.nativeOrder())
-        asFloatBuffer().apply {
-            put(QUADRANT_COORDINATES)
-            position(0)
+    private val quadrantCoordinatesBuffer = ByteBuffer
+        .allocateDirect(QUADRANT_COORDINATES.size * 4)
+        .run {
+            order(ByteOrder.nativeOrder())
+            asFloatBuffer().apply {
+                put(QUADRANT_COORDINATES)
+                position(0)
+            }
         }
-    }
 
-    /**
-     * Convert float array to float buffer
-     */
-    private val textureCoordinatesBuffer: FloatBuffer =
-        ByteBuffer.allocateDirect(TEXTURE_COORDINATES.size * 4).run {
+    private val textureCoordinatesBuffer = ByteBuffer
+        .allocateDirect(TEXTURE_COORDINATES.size * 4)
+        .run {
             order(ByteOrder.nativeOrder())
             asFloatBuffer().apply {
                 put(TEXTURE_COORDINATES)
@@ -106,105 +99,72 @@ class ScreenGLRenderer(private val screen: () -> Screen) : GLSurfaceView.Rendere
             }
         }
 
-    /**
-     * Convert short array to short buffer
-     */
-    private val drawOrderBuffer: ShortBuffer = ByteBuffer.allocateDirect(
-        DRAW_ORDER.size * 2).run {
-        order(ByteOrder.nativeOrder())
-        asShortBuffer().apply {
-            put(DRAW_ORDER)
-            position(0)
+    private val drawOrderBuffer = ByteBuffer
+        .allocateDirect(DRAW_ORDER.size * 2)
+        .run {
+            order(ByteOrder.nativeOrder())
+            asShortBuffer().apply {
+                put(DRAW_ORDER)
+                position(0)
+            }
         }
-    }
 
     private lateinit var pixelBuffer: ByteBuffer
 
-    /**
-     * Called when surface is created or recreated
-     */
     override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
-        // Set GL clear color to white.
         GLES20.glClearColor(255f, 255f, 255f, 1.0f)
 
-        // Prepare the rendering objects. This involves reading shaders, so may throw an IOException.
-        try {
-            val vertexShader =
-                ShaderUtil.loadGLShader(
-                    TAG,
-                    GLES20.GL_VERTEX_SHADER,
-                    vertexShader
-                )
-            val fragmentShader =
-                ShaderUtil.loadGLShader(
-                    TAG,
-                    GLES20.GL_FRAGMENT_SHADER,
-                    fragmentShader
-                )
+        val vertexShader = ShaderUtil.loadGLShader(
+            TAG,
+            GLES20.GL_VERTEX_SHADER,
+            vertexShader
+        )
+        val fragmentShader = ShaderUtil.loadGLShader(
+            TAG,
+            GLES20.GL_FRAGMENT_SHADER,
+            fragmentShader
+        )
 
-            program = GLES20.glCreateProgram()
-            GLES20.glAttachShader(program, vertexShader)
-            GLES20.glAttachShader(program, fragmentShader)
-            GLES20.glLinkProgram(program)
-            GLES20.glUseProgram(program)
+        program = GLES20.glCreateProgram()
+        GLES20.glAttachShader(program, vertexShader)
+        GLES20.glAttachShader(program, fragmentShader)
+        GLES20.glLinkProgram(program)
+        GLES20.glUseProgram(program)
 
-            ShaderUtil.checkGLError(TAG, "Program creation")
+        ShaderUtil.checkGLError(TAG, "Program creation")
 
-            //Quadrant position handler
-            quadPositionHandle = GLES20.glGetAttribLocation(program, "a_Position")
+        quadPositionHandle = GLES20.glGetAttribLocation(program, "a_Position")
+        texPositionHandle = GLES20.glGetAttribLocation(program, "a_TexCoord")
+        textureUniformHandle = GLES20.glGetUniformLocation(program, "u_Texture")
+        viewProjectionMatrixHandle = GLES20.glGetUniformLocation(program, "uVPMatrix")
 
-            //Texture position handler
-            texPositionHandle = GLES20.glGetAttribLocation(program, "a_TexCoord")
+        GLES20.glEnable(GLES20.GL_BLEND)
+        GLES20.glBlendFunc(GLES20.GL_ONE, GLES20.GL_ONE_MINUS_SRC_ALPHA)
 
-            //Texture uniform handler
-            textureUniformHandle = GLES20.glGetUniformLocation(program, "u_Texture")
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
+        GLES20.glGenTextures(textureUnit.size, textureUnit, 0)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureUnit[0])
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR)
+        pixelBuffer = ByteBuffer.allocateDirect(160 * 144 * 4).order(ByteOrder.nativeOrder())
 
-            //View projection transformation matrix handler
-            viewProjectionMatrixHandle = GLES20.glGetUniformLocation(program, "uVPMatrix")
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0)
 
-            //Enable blend
-            GLES20.glEnable(GLES20.GL_BLEND)
-            //Uses to prevent transparent area to turn in black
-            GLES20.glBlendFunc(GLES20.GL_ONE, GLES20.GL_ONE_MINUS_SRC_ALPHA)
-
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
-            GLES20.glGenTextures(textureUnit.size, textureUnit, 0)
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureUnit[0])
-            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR)
-            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR)
-
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0)
-            pixelBuffer = ByteBuffer.allocateDirect(160 * 144 * 4).order(ByteOrder.nativeOrder())
-
-            ShaderUtil.checkGLError(TAG, "Texture loading")
-        } catch (e: IOException) {
-        }
+        ShaderUtil.checkGLError(TAG, "Texture loading")
     }
 
-    /**
-     * Called after the surface is crated and whenever surface size changes
-     */
     override fun onSurfaceChanged(p0: GL10?, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
-
-        val ratio: Float = width.toFloat() / height.toFloat()
-
-        // this projection matrix is applied to object coordinates
-        // in the onDrawFrame() method
-        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
+        Matrix.orthoM(projectionMatrix, 0, -80f, 80f, -72f, 72f, -1f, 1f)
     }
 
     /**
      * Uses for draw the current frame
      */
     override fun onDrawFrame(p0: GL10?) {
-        // Use the GL clear color specified in onSurfaceCreated() to erase the GL surface.
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
 
-        // Set the camera position (View matrix)
-        Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, 3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
-
-        // Calculate the projection and view transformation
+        Matrix.setLookAtM(viewMatrix, 0, 80f, 72f, 1f, 80f, 72f, 0f, 0f, 1f, 0.0f)
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
 
         try {
@@ -287,6 +247,7 @@ actual fun RenderScreen(image: Screen) {
     }
 
     AndroidView(
+        modifier = Modifier.fillMaxSize(),
         factory = { context ->
             ScreenGLView(context, {
                 localImage
